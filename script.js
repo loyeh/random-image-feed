6508edf7c6a2e59981cc32336160953d7adc5892
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-01 21:01:58 +0330

    fixing some codes

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const container = document.querySelector(".container");
 const rowsNum = 5;
 
 function randomImage() {
-  for (let i = 1; i < rowsNum + 1; i++) {
+  for (let i = 0; i < rowsNum; i++) {
     const row = document.createElement("div");
     row.className = "row";
-    for (let j = rowsNum; j < rowsNum + 3; j++) {
+    for (let j = 0; j < 3; j++) {
       const image = document.createElement("img");
-      image.src = ` https://picsum.photos/300?random=${i * j}`;
+      image.src = ` https://picsum.photos/300?random=${i}${j}}`;
       row.appendChild(image);
     }
 
